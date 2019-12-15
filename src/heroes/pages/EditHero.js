b28bfc5495c8c9a25c478cc0b79e605b56b2490f
Devlin Duldulao <devlin.duldulao@inmeta.no>
2019-12-15 14:41:22 +0100

    updated

--- src/heroes/pages/EditHero.js
+++ src/heroes/pages/EditHero.js
@@ -3,6 +3,7 @@ import React from 'react'
 // TODO: import action creators
 
 export default function EditHero() {
+    // TODO: initialize dispatch
     return (
         <div>
             <h1>EditHero works!</h1>
