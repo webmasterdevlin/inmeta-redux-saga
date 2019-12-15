8b0585220bb78906858647432e2bd58ebc2d1697
Devlin Duldulao <devlin.duldulao@inmeta.no>
2019-12-15 12:05:28 +0100

    updated

--- src/heroes/pages/Heroes.js
+++ src/heroes/pages/Heroes.js
@@ -1,9 +1,18 @@
-import React from 'react'
-
+import React, {useEffect} from 'react'
+import {useDispatch, useSelector} from 'react-redux';
+import {fetchHeroes} from '../hero-actions'
 export default function Heroes() {
+    const dispatch = useDispatch();
+    const {heroes, isLoading} = useSelector(s => s.heroReducer);
+    useEffect(() => {
+        dispatch(fetchHeroes())
+    }, []);
     return (
         <div>
-            <h1>Heroes Works!</h1>
+            <h1>Super Heroes</h1>
+    <ul>{isLoading ?
+    <h2>Loading.. Please wait..</h2> :
+    heroes.map(h => <li key={h.id}>{`${h.firstName} ${h.lastName} is ${h.knownAs}`}</li>) }</ul>
         </div>
     )
 }
