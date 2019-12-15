7372005ff78e4e7749445c76b5f0978b538c9772
Devlin Duldulao <devlin.duldulao@inmeta.no>
2019-12-15 12:44:11 +0100

    updated

--- src/heroes/hero-saga.js
+++ src/heroes/hero-saga.js
@@ -11,6 +11,9 @@ import {
     FETCH_HEROES_REQUEST,
     FETCH_HEROES_SUCCESS,
     FETCH_HEROES_FAIL,
+    REMOVE_HERO_REQUEST,
+    REMOVE_HERO_FAIL,
+    REMOVE_HERO_SUCCESS
 } from './hero-actions';
 
 /* function generator implementations of Saga */
@@ -24,15 +27,25 @@ function* fetchingHeroes() {
         yield put({type: FETCH_HEROES_FAIL, payload: e.message})
     }
 }
+// renaming deconstructed object payload to id for readability
+function* removingHero({payload: id}) {
+    try {
+        yield deleteHeroById(id);
+        yield put({ type: REMOVE_HERO_SUCCESS, payload: id})
+    } catch (e) {
+        yield put({ type: REMOVE_HERO_FAIL, payload: e.message})
+    }
+}
 
 /* Saga watches the actions of hero-actions */
 function* watchFetchingHeroes() {
     yield takeEvery(FETCH_HEROES_REQUEST, fetchingHeroes)
 }
+function* watchRemovingHero() {
+    yield takeEvery(REMOVE_HERO_REQUEST, removingHero)
+}
 
 /* Saga sends all the watchers to the sagaMiddleware to run. */
 export function* heroSaga() {
-    yield all([watchFetchingHeroes(),
-    // other watching functions
-    ])
+    yield all([watchFetchingHeroes(), watchRemovingHero()])
 }
