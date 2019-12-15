6ad06c01b40b3c18fef017ff5f38895e14017aa5
Devlin Duldulao <devlin.duldulao@inmeta.no>
2019-12-15 12:34:48 +0100

    updated

--- src/heroes/hero-actions.js
+++ src/heroes/hero-actions.js
@@ -3,6 +3,10 @@ export const FETCH_HEROES_REQUEST = "FETCH_HEROES_REQUEST";
 export const FETCH_HEROES_SUCCESS = "FETCH_HEROES_SUCCESS";
 export const FETCH_HEROES_FAIL = "FETCH_HEROES_FAIL";
 
+export const DELETE_HERO_REQUEST = "DELETE_HERO_REQUEST";
+export const DELETE_HERO_SUCCESS = "DELETE_HERO_SUCCESS";
+export const DELETE_HERO_FAIL = "DELETE_HERO_FAIL";
+
 
 /*
 action creators for Saga, which uses function generator side effect.
@@ -12,3 +16,8 @@ Saga will watch these requests
 export const fetchHeroes = () => ({
     type: FETCH_HEROES_REQUEST
 });
+
+export const removeHero = (id) => ({
+    type: DELETE_HERO_REQUEST,
+    payload: id
+})
