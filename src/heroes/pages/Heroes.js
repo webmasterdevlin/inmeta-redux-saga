90893ed322e094baa4f72ff8d42cc1095339c8ee
Devlin Duldulao <devlin.duldulao@inmeta.no>
2019-12-15 10:16:09 +0100

    updated

---
+++ src/heroes/pages/Heroes.js
@@ -0,0 +1,9 @@
+import React from 'react'
+
+export default function Heroes() {
+    return (
+        <div>
+            <h1>Heroes Works!</h1>
+        </div>
+    )
+}
---
+++ src/root-router.js
@@ -0,0 +1,14 @@
+import React from 'react'
+import { Router, Redirect } from '@reach/router'
+import Heroes from './heroes/pages/Heroes'
+import EditHero from './heroes/pages/EditHero'
+
+const RootRouter = () => (
+    <Router>
+        <Redirect from="/" to="heroes" noThrow />
+        <Heroes path="heroes" />
+        <EditHero path="hero-detail/:id" />
+    </Router>
+)
+
+export default RootRouter;
