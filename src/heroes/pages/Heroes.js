e766c7bf78a9995b3c5c7bc133219809c57fd314
Devlin Duldulao <devlin.duldulao@inmeta.no>
2019-12-15 12:56:58 +0100

    updated

--- src/heroes/pages/Heroes.js
+++ src/heroes/pages/Heroes.js
@@ -1,6 +1,6 @@
 import React, {useEffect} from 'react'
 import {useDispatch, useSelector} from 'react-redux';
-import {fetchHeroes} from '../hero-actions';
+import {fetchHeroes, removeHero} from '../hero-actions';
 import Button from 'react-bootstrap/Button';
 export default function Heroes() {
     const dispatch = useDispatch();
@@ -10,12 +10,18 @@ export default function Heroes() {
     }, []);
     return (
         <div>
-            <h1>Super Heroes</h1>
-    <ul>{isLoading ?
-    <h2>Loading.. Please wait..</h2> :
-    heroes.map(h => <li key={h.id}>{`${h.firstName} ${h.lastName} is ${h.knownAs}`}
-    <Button variant="danger">Remove</Button>
-    </li>) }</ul>
+        <h1>Super Heroes</h1>
+        <ul>{isLoading ?
+        <h2>Loading.. Please wait..</h2> :
+             heroes.map(h => <li key={h.id}>
+                            {`${h.firstName} ${h.lastName} is ${h.knownAs}`}
+                            <Button onClick={() => dispatch(removeHero(h.id))}
+                            variant="danger">
+                                Remove
+                            </Button>
+                        </li>)
+                }</ul>
         </div>
     )
 }
+
